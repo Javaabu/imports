69998e38291b3fa7f2a3a0b1e58a282dfd22b2a3
Hussain Afeef <ibnahnajjaar@gmail.com>
2024-06-11 00:16:59 +0500

    udpate test workflow

---
+++ src/ImportsServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Javaabu\Imports;
+
+use Illuminate\Support\ServiceProvider;
+
+class ImportsServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot()
+    {
+        // declare publishes
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . '/../config/imports.php' => config_path('imports.php'),
+            ], 'imports-config');
+        }
+    }
+
+    /**
+     * Register the application services.
+     */
+    public function register()
+    {
+        // merge package config with user defined config
+        $this->mergeConfigFrom(__DIR__ . '/../config/imports.php', 'imports');
+    }
+}
